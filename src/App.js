6c5834ac9271168c909b1ff058c59994436d88c1
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-08-18 01:29:01 +0330

    add products section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './bootstrap-files/bootstrap.min.css'
 import './app.css'
 import Header from './components/header/Header';
 import About from './components/about/About';
+import Products from './components/products/Products';
 
 export default function App() {
   return (
     <div className="App">
      <Header />
      <About />
+     <Products />
     </div>
   );
 }
