bfadc516015bffb8efa497e1cf7d037e8abf05c6
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-08-17 19:54:58 +0330

    add about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './bootstrap-files/bootstrap-icons.min.css'
 import './bootstrap-files/bootstrap.min.css'
-import Header from './components/header/Header';
 import './app.css'
+import Header from './components/header/Header';
+import About from './components/about/About';
 
 export default function App() {
   return (
     <div className="App">
      <Header />
+     <About />
     </div>
   );
 }
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <div className="navbar-container text-white d-flex position-relative align-items-center">
-      <ul className="d-flex justify-content-between align-items-center my-3">
+      <ul className="d-none d-lg-flex justify-content-between align-items-center my-3">
         <li>خانه</li>
         <li>محصولات</li>
         <li>آلبوم عکس</li>
@@ -13,6 +13,7 @@ export default function Navbar() {
           </a>
         </li>
       </ul>
+      <i className="bi bi-list fs-1 d-lg-none"></i>
       <img src="imgs/logo.png" className="" />
     </div>
   )
